2e1822b806763d3c3f439a135f19b013b8bf700a
Jørgen Vatle <Jorgen@WcAServices.net>
2018-12-09 10:50:05 +0200

    Add JSDoc to Storage class.

--- src/Storage.ts
+++ src/Storage.ts
@@ -4,12 +4,23 @@ import Filesystem from 'fs';
 
 export default class Storage {
 
+    /**
+     * Storage path.
+     */
     protected storagePath: string;
 
+    /**
+     * Storage constructor.
+     */
     public constructor() {
         this.storagePath = Path.join(OperatingSystem.homedir(), 'desktop-spotlight');
     }
 
+    /**
+     * Store file.
+     *
+     * @param file
+     */
     public store(file: { name: string, blob: Blob }) {
         Filesystem.writeFile(Path.join(this.storagePath, file.name), file.blob, (err: any) => {
             if (err) console.log({ err });
