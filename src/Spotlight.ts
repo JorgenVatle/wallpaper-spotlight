333168b7f33d57d8878a4f93574bf5346e01e418
Jorgen Vatle <jorgen@wcaservices.net>
2018-12-09 04:59:12 +0200

    Correct inferred `any` type in Spotlight.ts

--- src/Spotlight.ts
+++ src/Spotlight.ts
@@ -1,8 +1,8 @@
-import Axios from 'axios';
+import Axios, { AxiosInstance } from 'axios';
 
 export default class Spotlight {
 
-    protected api;
+    protected api: AxiosInstance;
 
     constructor() {
         this.api = Axios.create({
