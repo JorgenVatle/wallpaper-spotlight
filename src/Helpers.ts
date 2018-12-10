f63d804cd84ae664bde21d709658a31a07d6a430
Jørgen Vatle <Jorgen@WcAServices.net>
2018-12-10 11:09:34 +0200

    Add array sample method to Helpers.

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -10,4 +10,11 @@ export default new class Helpers {
         return Math.floor(Math.random() * ( max - min + 1 ) + min);
     }
 
+    /**
+     * Sample a random entry from the given array.
+     * @param array
+     */
+    sample(array: Array<any>) {
+        return array[this.random(1, array.length) - 1];
+    }
 }
