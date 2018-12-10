3d41506e6934a431c4f0cc616fe4b8a874ed4fef
Jørgen Vatle <Jorgen@WcAServices.net>
2018-12-10 11:38:27 +0200

    Add Unsplash user interface to Image interface.

--- src/assets/Image.interface.ts
+++ src/assets/Image.interface.ts
@@ -27,5 +27,36 @@ interface UnsplashImageData {
     liked_by_user: boolean,
     current_user_collections: Array<any>,
     slug: null | string,
-    user: any,
+    user: UnsplashUser,
+}
+
+interface UnsplashUser {
+    id: string,
+    updated_at: string,
+    username: string,
+    name: string,
+    first_name: string,
+    last_name: string,
+    twitter_username: string | null,
+    portfolio_url: string | null,
+    location: string,
+    instagram_username: string,
+    total_collections: number,
+    total_likes: number,
+    total_photos: number,
+    accepted_tos: boolean,
+    links: {
+        self: string,
+        html: string,
+        photos: string,
+        likes: string,
+        portfolio: string,
+        following: string,
+        followers: string,
+    },
+    profile_image: {
+        small: string,
+        medium: string,
+        large: string,
+    },
 }
