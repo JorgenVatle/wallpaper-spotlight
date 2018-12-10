867ffa13a6cf18301fad6f6e84c8f9e077ba0e69
Jørgen Vatle <Jorgen@WcAServices.net>
2018-12-10 10:15:48 +0200

    Add spinners to get command.

--- src/commands/get.ts
+++ src/commands/get.ts
@@ -1,4 +1,5 @@
 import { Command } from '@oclif/command';
+import CLI from 'cli-ux';
 import Spotlight from '../Spotlight';
 
 export default class Get extends Command {
@@ -12,7 +13,12 @@ export default class Get extends Command {
      * Fetch wallpaper.
      */
     async run() {
+        CLI.action.start('Locating wallpaper');
         const wallpaper = await Spotlight.wallpaper;
+        CLI.action.stop();
+
+        CLI.action.start('Downloading wallpaper');
         await Spotlight.storeImage(wallpaper.data);
+        CLI.action.stop(wallpaper.data.description);
     }
 }
