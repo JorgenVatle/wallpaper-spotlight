fedd23c335f3bf2e136af941828e9ce54d8d0393
Jørgen Vatle <Jorgen@WcAServices.net>
2018-12-10 11:50:56 +0200

    Use Image instance to save images in get command.

--- src/commands/get.ts
+++ src/commands/get.ts
@@ -1,6 +1,5 @@
 import { Command } from '@oclif/command';
 import CLI from 'cli-ux';
-import Spotlight from '../Spotlight';
 import Wallpapers from '../Wallpapers';
 
 export default class Get extends Command {
@@ -19,7 +18,7 @@ export default class Get extends Command {
         CLI.action.stop();
 
         CLI.action.start('Downloading wallpaper');
-        await Spotlight.storeImage(wallpaper);
-        CLI.action.stop(wallpaper.description);
+        await wallpaper.store();
+        CLI.action.stop(wallpaper.image.description || undefined);
     }
 }
