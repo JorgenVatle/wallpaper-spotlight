580b374697c65829cf926a61930bca61c9afd8db
Jørgen Vatle <Jorgen@WcAServices.net>
2018-12-10 14:27:59 +0200

    Trigger push notification on wallpaper command completion.

--- src/commands/wallpaper.ts
+++ src/commands/wallpaper.ts
@@ -1,5 +1,6 @@
 import { Command } from '@oclif/command';
 import CLI from 'cli-ux';
+import * as Notifier from 'node-notifier';
 import Wallpapers from '../Wallpapers';
 
 export default class Wallpaper extends Command {
@@ -20,5 +21,10 @@ export default class Wallpaper extends Command {
         CLI.action.start('Downloading and applying wallpaper');
         await wallpaper.setAsWallpaper();
         CLI.action.stop(wallpaper.image.description || undefined);
+
+        Notifier.notify({
+            title: `Photograph by ${wallpaper.image.user.name}`,
+            message: 'You can view their portfolio on Unsplash!',
+        })
     }
 }
