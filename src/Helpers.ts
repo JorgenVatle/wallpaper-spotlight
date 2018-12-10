3f5f0e79f187e31a1de95a87843954a17861cb3a
Jørgen Vatle <Jorgen@WcAServices.net>
2018-12-10 11:07:53 +0200

    Add JSDoc to random() method.

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -1,5 +1,11 @@
 export default new class Helpers {
 
+    /**
+     * Create a random integer within the given range.
+     *
+     * @param min
+     * @param max
+     */
     random(min: number, max: number) {
         return Math.floor(Math.random() * ( max - min + 1 ) + min);
     }
